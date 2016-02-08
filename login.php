8e2a3d344a1a4ade57242ce0fcdd9e8c9e9f5c80
xblaze <MilBlazek@gmail.com>
2016-02-08 15:50:29 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,9 @@
 <?php
-
+  require "./connect.php";
+  
+  $login = $_POST['username'];
+  $pass = md5($_POST['password']);
+  
+  $sql = "SELECT * FROM 'Users' WHERE user='$login' AND pass='$pass'";
+  $result = mysql_query($sql);
 ?>
